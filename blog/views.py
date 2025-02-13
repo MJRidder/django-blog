c54f51f5cb64b4d3a998f2de435728370d4d66d0
Maarten Ridder <maartenj.ridder@gmail.com>
2025-02-13 14:49:26 +0000

    EOD commit

--- blog/views.py
+++ blog/views.py
@@ -33,6 +33,7 @@ def post_detail(request, slug):
     comment_count = post.comments.filter(approved=True).count()
 
     if request.method == "POST":
+        print("Received a POST request abcd")
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -45,7 +46,7 @@ def post_detail(request, slug):
             )
 
     comment_form = CommentForm()
-
+    print("About to render template abcd")
     return render(
         request,
         "blog/post_detail.html",
