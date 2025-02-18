f645883b195a915cd5c9c7edfc4d09105ed83ee4
Maarten Ridder <maartenj.ridder@gmail.com>
2025-02-18 13:16:17 +0000

    added all docstrings

--- about/admin.py
+++ about/admin.py
@@ -5,6 +5,9 @@ from .models import About, CollaborateRequest
 
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
+    """
+    Adds rich-text editing of content in admin
+    """.0
     summernote_fields = ('content',)
 
 # Note: admin.ModelAdmin is the standard way of registering
@@ -17,5 +20,7 @@ class AboutAdmin(SummernoteModelAdmin):
 
 @admin.register(CollaborateRequest)
 class CollaborateRequestAdmin(admin.ModelAdmin):
-
+    """
+    Lists message and read fields for display in admin
+    """
     list_display = ('message', 'read',)
