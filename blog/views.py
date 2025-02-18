a6a8df7d41dee5839c6e2c42847ac36302aaf165
Maarten Ridder <maartenj.ridder@gmail.com>
2025-02-18 10:48:01 +0000

    initial tidying up steps taken

--- blog/views.py
+++ blog/views.py
@@ -22,6 +22,13 @@ def post_detail(request, slug):
 
     ``post``
         An instance of :model:`blog.Post`.
+    ``comments``
+        All approved comments related to the post
+    ``comment_count``
+        The ammount of approved comments related to the post
+    ``comment_form``
+        An instance of :form:`blog.CommentForm`.
+    **Template:**
 
     **Template:**
 
@@ -34,7 +41,6 @@ def post_detail(request, slug):
     comment_count = post.comments.filter(approved=True).count()
 
     if request.method == "POST":
-        print("Received a POST request abcd")
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -47,7 +53,6 @@ def post_detail(request, slug):
             )
 
     comment_form = CommentForm()
-    print("About to render template abcd")
     return render(
         request,
         "blog/post_detail.html",
@@ -61,7 +66,16 @@ def post_detail(request, slug):
 
 def comment_edit(request, slug, comment_id):
     """
-    view to edit comments
+    view an individual comment for edit.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+    ``comment``
+        A single comment related to the post
+    ``comment_form``
+        An instance of :form:`blog.CommentForm`.
     """
     if request.method == "POST":
 
@@ -84,7 +98,14 @@ def comment_edit(request, slug, comment_id):
 
 def comment_delete(request, slug, comment_id):
     """
-    view to delete comment
+    Delete an individual comment.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+    ``comment``
+        A single comment related to the post
     """
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
